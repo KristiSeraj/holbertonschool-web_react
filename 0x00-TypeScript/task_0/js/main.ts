e46c684f030508a0d028653e29f2adad430443fa
KristiSeraj <serajkristi@gmail.com>
2023-05-02 11:10:46 +0200

    main ts

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,37 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string
+}
+
+const student1: Student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 20,
+    location: 'Albania'
+};
+const student2: Student = {
+    firstName: 'Bob',
+    lastName: 'Builder',
+    age: 54,
+    location: 'USA'
+};
+
+const studentList: Student[] = [student1, student2];
+
+const tb = document.createElement('table');
+
+studentList.forEach((student) => {
+    const row = document.createElement('tr');
+
+    const fnCell = document.createElement('td');
+    fnCell.textContent = student.firstName;
+    row.appendChild(fnCell);
+
+    const locationCell = document.createElement('td');
+    locationCell.textContent = student.location;
+    row.appendChild(locationCell);
+
+    tb.appendChild(row);
+})
