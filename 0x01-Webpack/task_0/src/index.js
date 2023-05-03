63a1d7e21a99710bcdd41e8005f2f285e544f618
KristiSeraj <serajkristi@gmail.com>
2023-05-03 10:23:39 +0200

    src js file

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+let p1 = $('<p></p>').text('Holberton Dashboard');
+let p2 = $('<p></p>').text('Dashboard data for the students');
+let p3 = $('<p></p>').text('Copyright - Holberton School');
+
+$('body').append(p1, p2, p3);
