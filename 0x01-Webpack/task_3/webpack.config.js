05fc8422fc634d35d6846f04824560c26da583c8
KristiSeraj <serajkristi@gmail.com>
2023-05-05 16:49:24 +0200

    config files

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    header: "./modules/header/header.js",
+    body: "./modules/body/body.js",
+    footer: "./modules/footer/footer.js"
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  performance: {
+    maxAssetSize: 1000000,
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin()
+  ],
+  devServer: {
+    port: 8564,
+    contentBase: path.join(__dirname, 'public'),
+    compress: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader','css-loader'],
+      },
+      {
+        test: /\.(jpg|gif|png|jpeg|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
+
