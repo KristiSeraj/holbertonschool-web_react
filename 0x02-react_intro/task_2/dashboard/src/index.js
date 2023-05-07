5d2038a428685baf5a55ec2ac83a007cc62fe21e
KristiSeraj <serajkristi@gmail.com>
2023-05-07 12:52:17 +0200

    fixex

--- 0x02-react_intro/task_2/dashboard/src/index.js
+++ 0x02-react_intro/task_2/dashboard/src/index.js
@@ -6,10 +6,10 @@ import Notification from './Notification';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <div className='root-notification'>
       <Notification />
     </div>
+    <App />
   </React.StrictMode>
 );
 
