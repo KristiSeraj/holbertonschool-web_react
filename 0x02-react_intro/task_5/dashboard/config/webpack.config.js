f32bf7306c39671818424ca3f51faa9680b797d3
KristiSeraj <serajkristi@gmail.com>
2023-05-10 11:44:27 +0200

    webpack config

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -2,14 +2,25 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
     devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, '../dist')
     },
     devServer: {
         hot: true,
+        static: {
+            directory: path.join(__dirname, "../dist"),
+        },
         compress: true,
         port: 8564
     },
@@ -24,6 +35,9 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                   loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                  }
                 },
             },
             {
