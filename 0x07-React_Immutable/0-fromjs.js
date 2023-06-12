23052df78f4d1890e5925178bf3e4df96f594ef5
KristiSeraj <serajkristi@gmail.com>
2023-06-12 10:50:31 +0200

    fixed

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,8 +1,8 @@
-import { fromJS } from "./node_modules/immutable/dist/immutable";
+import { fromJS } from './node_modules/immutable/dist/immutable';
 
 const getImmutableObject = (object) => {
   const map = fromJS(object);
   return map;
-}
+};
 
 export default getImmutableObject;
