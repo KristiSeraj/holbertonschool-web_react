98434393baa5dab753deeccb0e2853475ec2be7d
KristiSeraj <serajkristi@gmail.com>
2023-06-12 10:44:24 +0200

    fromjs

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,7 +1,8 @@
-const { fromJS } = require('immutable');
+import { fromJS } from "./node_modules/immutable/dist/immutable";
+
 const getImmutableObject = (object) => {
-    const map = fromJS(object);
-    return map;
+  const map = fromJS(object);
+  return map;
 }
 
 export default getImmutableObject;
