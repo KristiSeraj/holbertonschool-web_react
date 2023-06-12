633894be88ec04004ac16a6c867b091a1742e040
KristiSeraj <serajkristi@gmail.com>
2023-06-12 12:32:23 +0200

    fix

---
+++ 0x07-React_Immutable/2-nested.js
@@ -0,0 +1,18 @@
+export default function accessImmutableObject(object, array) {
+    if (!object || !Array.isArray(array) || array.length === 0) {
+        return undefined;
+    }
+
+    let value = object;
+
+    for (let i = 0; i < array.length; i++) {
+        let key = array[i];
+        if (value && value.hasOwnProperty(key)) {
+            value = value[key];
+        } else {
+            return undefined;
+        }
+    }
+
+    return value;
+}
