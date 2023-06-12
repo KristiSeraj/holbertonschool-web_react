0efca0f4eeb355b66c9484b722a7a4bbeba9410f
KristiSeraj <serajkristi@gmail.com>
2023-06-12 14:42:40 +0200

    fix

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,18 +1,11 @@
+import { fromJS } from './node_modules/immutable/dist/immutable';
+
 export default function accessImmutableObject(object, array) {
   if (!object || !Array.isArray(array) || array.length === 0) {
     return undefined;
   }
 
-  let value = object;
+  const nested = fromJS(object);
 
-  for (let i = 0; i < array.length; i += 1) {
-    const key = array[i];
-    if (value && Object.prototype.hasOwnProperty.call(value, key)) {
-      value = value[key];
-    } else {
-      return undefined;
-    }
-  }
-
-  return value;
+  return nested.getIn(array);
 }
