03a728e51f02999a92387fea5a72ebdad97b250c
KristiSeraj <serajkristi@gmail.com>
2023-06-13 11:24:37 +0200

    merge and concat

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -2,11 +2,12 @@ import { List, Map } from './node_modules/immutable/dist/immutable';
 
 export function concatElements(page1, page2) {
   const list1 = List([]);
-  return list1.concat(page1, page2);
-};
+  const list2 = list1.concat(page1, page2);
+  return list2;
+}
 
 export function mergeElements(page1, page2) {
   const newMap = Map(page1).mergeDeep(page2);
   const newList = List(newMap.valueSeq());
   return newList;
-};
+}
