00c83c24cad1fe6139ef4dee8724537ffc9bba68
KristiSeraj <serajkristi@gmail.com>
2023-06-19 11:07:15 +0200

    immutable object

---
+++ 0x09-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -0,0 +1,26 @@
+import { uiReducer, initialState} from "./uiReducer";
+import { LOGIN_FAILURE } from "../actions/uiActionTypes";
+import { Map } from 'immutable';
+
+describe('UI Reducer', () => {
+    describe('testing state', () => {
+        it('should test state with undefined', () => {
+            const result = uiReducer(undefined, {});
+
+            expect(result.toJS()).toEqual(initialState)
+        });
+        it('should test state with undefined and a type', () => {
+            const result = uiReducer(undefined, { type: 'UNSELECT_COURSE' });
+
+            expect(result.toJS()).toEqual(initialState);
+        });
+        it('should test state with undefined and a type', () => {
+            const result = uiReducer(undefined, { type: LOGIN_FAILURE });
+
+            expect(result.toJS()).toEqual({
+                ...initialState,
+                isUserLoggedIn: false
+            });
+        });
+    });
+});
